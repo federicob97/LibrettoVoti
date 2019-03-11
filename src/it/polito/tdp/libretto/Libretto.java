c5bdae42c0bde5d73aa4e28eef34cae40cff7e6b
feder <feder@LAPTOP-EB4IGPN3>
2019-03-11 14:30:10 +0100

    Aggiornata classe Libretto con lista voti

---
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -0,0 +1,17 @@
+package it.polito.tdp.libretto;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class Libretto {
+
+	private List<Voto> voti;
+	
+	public Libretto() {
+		this.voti = new ArrayList<Voto>();
+	}
+	
+	public void add(Voto v) {
+		
+	}
+}
